2653b5c4eb15d79ec62f2f4ca6d8332fe00aba47
ragnarady <ragnarady@gmail.com>
2015-08-30 19:01:53 +0300

    commit 3008150

--- src/com/ragnarady/game/puyo/utils/ImageUtils.java
+++ src/com/ragnarady/game/puyo/utils/ImageUtils.java
@@ -2,24 +2,20 @@ package com.ragnarady.game.puyo.utils;
 
 import java.awt.*;
 
-/**
- * Created by Arlabunakty on 8/29/2015.
- */
 public class ImageUtils {
 
-    public static final String SRC_MAIN_RES_IMAGES = "res\\images";
-
+    public static final String SRC_RES_IMAGES = "res\\images";
+   
     /**
-     * Loads images from
-     * @return
+     * Loads Puyo images resource
      */
-    static public Image[] loadImages() {
+   static public Image[] loadImages() {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         return new Image[] {
-            toolkit.getImage(SRC_MAIN_RES_IMAGES + "\\puyo_blue.png"),
-            toolkit.getImage(SRC_MAIN_RES_IMAGES + "\\puyo_red.png"),
-            toolkit.getImage(SRC_MAIN_RES_IMAGES + "\\puyo_yellow.png"),
-            toolkit.getImage(SRC_MAIN_RES_IMAGES + "\\puyo_green.png")
+        	toolkit.getImage(SRC_RES_IMAGES + "\\puyo_blue.png"),
+            toolkit.getImage(SRC_RES_IMAGES + "\\puyo_red.png"),
+            toolkit.getImage(SRC_RES_IMAGES + "\\puyo_yellow.png"),
+            toolkit.getImage(SRC_RES_IMAGES + "\\puyo_green.png")
         };
     }
 }
